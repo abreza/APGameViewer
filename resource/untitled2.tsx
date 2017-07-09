1339dd3946fc715f9b5d1c11cd6784198ee3ffb1
reza <a.re.morteza@gmail.com>
2017-07-10 02:05:26 +0430

    change some pictures

---
+++ resource/untitled2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Building0" tilewidth="32" tileheight="32" tilecount="80" columns="8">
+ <image source="Terrain_Sprites/MapStuff/Building0.png" width="265" height="321"/>
+</tileset>
